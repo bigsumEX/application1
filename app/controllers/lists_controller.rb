58b873fe83bd1bf0ba66aee87cea34b8ce828c40
bigsumEX <ren.owa2001@gmail.com>
2022-07-24 12:17:41 +0000

    [finish] 3-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,18 @@
 class ListsController < ApplicationController
   def new
+    #Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @list = List.new
   end
-
+  
+  def create
+    #1,2 :フォームに書かれたデータを受け取り新規登録するためのインスタンス
+    list = List.new(list_params)
+    #3 :データをデータベースに格納するためのsaveメソッド実行
+    list.save
+    #4 :トップ画面へリダイレクト
+    redirect_to  '/top'
+  end
+  
   def index
   end
 
@@ -9,5 +20,12 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+  
+  private
+  #ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
+    #データの絞り込み
   end
 end
