2b5ac532a047188a00c4bcc39be85b1f061ff5c7
bigsumEX <ren.owa2001@gmail.com>
2022-07-25 05:31:31 +0000

    [finish]3-5,3-6

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'lists' => 'lists#index'
   #.../lists/1や.../lists/3に該当
   get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/edit'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
