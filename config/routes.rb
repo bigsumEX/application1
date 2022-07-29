38f643b92ad609bdea518f07499ce598d8d43e91
bigsumEX <ren.owa2001@gmail.com>
2022-07-29 05:40:01 +0000

    [finish] 3-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  post 'lists' => 'lists#create'
-  get 'list/edit'
-  get 'top' => 'homes#top'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  #.../lists/1や.../lists/3に該当
-  get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
+ get 'top' => 'homes#top'
+ resources :lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
